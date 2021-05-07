cf512ac6eeea482a35c7d50d2e44eebef1afdaa2
Amr Nabeel <amr.nabeel@gmail.com>
2021-05-07 14:03:35 +0200

    added useLocation to remove Add task button on about screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
+import { useLocation } from "react-router-dom";
 import { Button } from "./Button";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
   return (
-    <header className="header">
+    <header className='header'>
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "blue" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "blue" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
